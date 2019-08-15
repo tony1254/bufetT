661d7130ab2fb12723a5b4288427f41dd6e2e85e
Tony Garcia <lgarcia-1254@outlook.com>
2019-08-14 23:57:53 -0600

    bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,114 @@
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Google Fonts -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css"> -->
+    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css"> -->
+
+
+    {{-- <link rel="shortcut icon" href="{{url("/content/favicon/favicon.png")}}" type="image/x-icon"> --}}
+    {{-- <link rel="icon" href="{{url("/content/favicon/favicon.png")}}" type="image/x-icon"> --}}
+    <link rel="apple-touch-icon" sizes="57x57" href="{{url("/content/favicon/apple-icon-57x57.png")}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{url("/content/favicon/apple-icon-60x60.png")}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{url("/content/favicon/apple-icon-72x72.png")}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{url("/content/favicon/apple-icon-76x76.png")}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{url("/content/favicon/apple-icon-114x114.png")}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{url("/content/favicon/apple-icon-120x120.png")}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{url("/content/favicon/apple-icon-144x144.png")}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{url("/content/favicon/apple-icon-152x152.png")}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{url("/content/favicon/apple-icon-180x180.png")}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{url("/content/favicon/android-icon-192x192.png")}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{url("/content/favicon/favicon-32x32.png")}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{url("/content/favicon/favicon-96x96.png")}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url("/content/favicon/favicon-16x16.png")}}">
+    <link rel="manifest" href="{{url("/content/favicon/manifest.json")}}">
+    <meta name="msapplication-TileColor" content="#3f51b5">
+    <meta name="msapplication-TileImage" content="{{url("/content/favicon/ms-icon-144x144.png")}}">
+    <meta name="theme-color" content="#3f51b5">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous"> -->
+</head>
+
+<body>
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Fixed navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+
+    <!-- Begin page content -->
+    <main role="main" id="app" class="container">
+      <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+
+
+
+      <button type="button" class="btn btn-outline-primary --orange">Primary</button>
+      <button class="btn btn-material-lime">Light Green button</button>
+
+<!-- Large button groups (default and split) -->
+<div class="btn-group">
+  <button class="btn btn-secondary  btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Large button
+  </button>
+  <div class="dropdown-menu">
+    ...
+  </div>
+</div>
+
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="{{ asset('js/app.js')}}"></script>
+    <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+  </body>
+</html>
